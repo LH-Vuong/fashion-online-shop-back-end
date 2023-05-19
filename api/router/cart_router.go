5aa579f8210dedb5578397f5ce272540e350db25
unknown <luonghuuvuong16b@gmail.com>
2023-05-19 15:57:03 +0700

    integrate middleware for cart

--- api/router/cart_router.go
+++ api/router/cart_router.go
@@ -12,12 +12,17 @@ import (
 func InitCartRouter(s *gin.Engine, c *dig.Container) {
 	err := c.Invoke(func(cartService service.CartService) {
 		controller := controller.CartController{Service: cartService}
-		s.GET("api/cart", middleware.DeserializeUser(), controller.Get)
-		s.PUT("api/cart", middleware.DeserializeUser(), controller.AddMany)
-		s.POST("api/cart", middleware.DeserializeUser(), controller.Update)
-		s.DELETE("api/cart", middleware.DeserializeUser(), controller.DeleteMany)
-		s.DELETE("api/cart/product_id", middleware.DeserializeUser(), controller.Delete)
-		s.GET("api/cart/checkout", middleware.DeserializeUser(), controller.CheckOut)
+		cartRouter := s.Group("api/cart")
+
+		{
+			cartRouter.Use(middleware.DeserializeUser())
+			cartRouter.GET("", controller.Get)
+			cartRouter.PUT("", controller.AddMany)
+			cartRouter.POST("", controller.Update)
+			cartRouter.DELETE("", controller.DeleteMany)
+			cartRouter.DELETE("/product_id", controller.Delete)
+			cartRouter.GET("/checkout", controller.CheckOut)
+		}
 	})
 	if err != nil {
 		panic(err)
